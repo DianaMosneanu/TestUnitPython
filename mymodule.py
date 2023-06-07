848d1a86f2837ce5e08d80a28240c854efd66b7a
DianaMosneanu <121347677+DianaMosneanu@users.noreply.github.com>
2023-06-07 09:10:52 +0300

    Create mymodule.py
    Aplicația va face referire la informații de bază, cum ar fi numele, e-mailul și ID-ul

---
+++ mymodule.py
@@ -0,0 +1,13 @@
+def square(number):
+"""
+This function returns the square of given number
+"""
+
+return number ** 2
+
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
