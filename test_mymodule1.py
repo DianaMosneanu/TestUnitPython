f033ff7f6f4ae0e5c043067885ebf4a2d2098ad8
DianaMosneanu <121347677+DianaMosneanu@users.noreply.github.com>
2023-06-07 15:01:49 +0300

    Update test_mymodule1.py
     test cases for these scenarios.
    When 2 and 4 are given as input the output must be 6.
    
    When 0 and 0 are given as input the output must be 0.
    
    When 2.3 and 3.6 are given as input the output must be 5.9.
    
    When the strings ‘hello’ and ‘world’ are given as input the output must be ‘helloworld’.
    
    When 2.3000 and 4.300 are given as input the output must be 6.6.
    
    When -2 and -2 are given as input the output must not be 0. (Hint : Use assertNotEqual)

--- test_mymodule1.py
+++ test_mymodule1.py
@@ -1,5 +1,14 @@
-def add(a,b):
-     """
-     This function returns the sum of the given numbers
-     """
-return a + b
+import unittest
+
+from mymodule import add
+
+class TestAdd(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(add(2,4), 6) # test when 2 and 4 are given as input the output is 6.
+        self.assertEqual(add(0,0), 0) # test when 0 and 0 are given as input the output is 0.
+        self.assertEqual(add(2.3,3.6),5.9 ) # test when 2.3 and 3.6 are given as input the output 5.9.
+
+        self.assertEqual(add('hello','world'), 'helloworld') # test when 'hell' and 'world' are given as input the output 'helloworld'.
+        self.assertEqual(add(2.3000,4.300), 6.6) # test when 2.3000 and 4.300 are given as input the output 6.6.
+        self.assertNotEqual(add(-2,-2), 0) # test when -2 and-2 are given as input the output are not 0.
+        
